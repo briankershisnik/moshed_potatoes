88ca2e16730ab4da91f6bfafca4cdc2044aaf2ff
abrah moh <srjsml12@gmail.com>
2022-07-09 14:30:22 -0600

    debugged

--- client/src/App.js
+++ client/src/App.js
@@ -2,25 +2,29 @@ import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import Home from './components/shared/home/Home'
 import Nomatch from './components/shared/Nomatch'
-import Navbar from './components/shared/Navbar';
-import Login from './components/auth/Login';
-import Register from './components/auth/Register';
-import ProtectedRoute from './components/auth/ProtectedRoute'
-import Movies from './components/movies/Movies'
+import Navbar from './components/shared/Navbar'
+import Login from './components/auth/Login'
+import Register from './components/auth/Register'
+import ProtectedRoute from './components/auth/ProtectedRoute';
+import FetchUser from './components/auth/FetchUser';
+import Movies from './components/movies/Movies';
 
 function App() {
   return (
     <>
-     <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path='/' element={<ProtectedRoute />}>
-          <Route path='movies' element={<Movies />} />
-        </Route>
-        <Route path='/login' element={<Login />} />
-        <Route path='/register' element={<Register />} />
-        <Route path="/*" element={<Nomatch />} />
-      </Routes>
+      <Navbar />
+      <FetchUser>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/" element={<ProtectedRoute />}>
+          <Route path="/movies" element={<Movies />} />
+          </Route>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/*" element={<Nomatch />} />
+        </Routes>
+      </FetchUser>
+
     </>
   )
 }
